176fa17496c4e8c92159c6718951b5ac35baec67
frochera <frochera@uci.edu>
2024-03-05 12:12:44 -0800

    Initial commit

---
+++ js/intro.js
@@ -0,0 +1,19 @@
+//resource link: https://www.w3schools.com/jquery/default.asp
+
+$(document).ready(function(){
+    $("#q1").click(function(){
+      $("#a1").slideToggle("slow");
+    });
+
+    $("#q2").click(function(){
+        $("#a2").slideToggle("slow");
+      });
+
+    $("#q3").click(function(){
+        $("#a3").slideToggle("slow");
+    });
+
+    $("#q4").click(function(){
+        $("#a4").slideToggle("slow");
+    });
+});
